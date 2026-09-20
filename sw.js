d622e28d59c78a6d74b01fb02284e34e1b677bc0
GZod56 <vikram.vijaay@gmail.com>
2026-09-20 17:41:03 -0400

    Refresh cache for recipe photos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='mp-v11';
+const V='mp-v12';
 const SHELL=['./','./index.html','./engine.js','./shopping.js','./sync.js','./recipes-ui.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil((async()=>{
   const c=await caches.open(V);
